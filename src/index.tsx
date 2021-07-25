50ed544a520022feef3c6230fd134d4a31600f9f
L <my917@qq.com>
2021-07-25 19:18:46 +0800

    使用 react-snap 预渲染，提高首屏加载速度

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const root = document.getElementById('root');
+
+if(root?.hasChildNodes()){
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root
+  );
+}else{
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root
+  );
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
